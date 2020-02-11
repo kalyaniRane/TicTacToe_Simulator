56fcae313d8664542dfbdc561808af4e53bd5885
kalyaniRane <kalyanirane19@gmail.com>
2020-02-11 18:12:01 +0530

    ADDED : Function To Assign a Letter X Or O To Player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 echo "Welcome to TicTacToe Problem !!"
-#Reset Board
+
 board=(1 2 3 4 5 6 7 8  9)
 
 #Function to display the board
@@ -11,4 +11,14 @@ function viewBoard(){
 		echo " ${board[i]} | ${board[i+1]} | ${board[i+2]} "
 		echo " ---------"
 	done
+}
+
+#Function to assign a symbol to player
+function assigningSymbol(){
+	if [[ $((RANDOM % 2)) -eq 0 ]]
+	then
+			player='X'
+	else
+			player='O'
+	fi
 }
