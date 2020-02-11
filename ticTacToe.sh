5bac8a683778059f8d4b3de28a7100135e1f9189
kalyaniRane <kalyanirane19@gmail.com>
2020-02-11 18:05:07 +0530

    ADDED : Resetting Board For PlayerTo Start Fresh Game

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to TicTacToe Problem !!"
+#Reset Board
+board=(1 2 3 4 5 6 7 8  9)
+
+#Function to display the board
+function viewBoard(){
+	for ((i=0;i<9;i+=3))
+	do
+		echo " ${board[i]} | ${board[i+1]} | ${board[i+2]} "
+		echo " ---------"
+	done
+}
