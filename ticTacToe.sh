a2c11e4c06bd54fa72e8f448fc4f9eadb1d540c8
kalyaniRane <kalyanirane19@gmail.com>
2020-02-12 10:54:22 +0530

    ADDED : Function To Check Player Is Win Or Tie

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,6 +2,9 @@
 
 echo "Welcome to TicTacToe Problem !!"
 
+#variable
+counter=0
+
 board=(1 2 3 4 5 6 7 8 9)
 
 #Function to display the board
@@ -17,25 +20,75 @@ function viewBoard(){
 function checkWhoPlayFirst(){
 	if [[ $((RANDOM % 2)) -eq 0 ]]
 	then
-			player='X'
-			echo "Player $player play first"
+			sign='X'
 	else
-			player='O'
-			echo "Player $player play first"
+			sign='O'
 	fi
 }
 
 #Function to check the cell is valid to enter symbol or not
 function checkValidCell(){
-	read -p "Enter a position to mark a sign: " position
 	if [[ ${board[position-1]} -eq $position ]]
 	then
-			viewBoard
+			available=1
 	else
-			echo "Choose another position"
+			available=0
+	fi
+	echo $available
+}
+
+#Function to play a player
+function playerTurn(){
+	if [[ $counter -lt 9 ]]
+	then
+		read -p "Enter a position to mark a sign: " position
+		if [[ $(checkValidCell) -eq 1 ]]
+		then
+				board[position-1]=$1
+				((counter++))
+				viewBoard
+				checkWin
+		else
+				echo "Choose Another Position"
+		fi
+	else
+		echo "Game Over"
+	fi
+	viewBoard
+}
+
+#Function to check winning condition for rows and columns
+function checkWin(){
+	j=0
+	for((i=0;i<9;i=i+3))
+	do
+		if [[ ${board[((i))]} == ${board[((i+1))]} && ${board[((i+1))]} == ${board[((i+2))]} ]]
+		then
+				exit
+		elif [[ ${board[((j))]} == ${board[((j+3))]} && ${board[((j+3))]} == ${board[((j+6))]} ]]
+		then
+				exit
+		fi
+		((j++))
+	done
+	checkDiagonal $sign
+	playerTurn  $sign
+}
+
+#Function to check diagonal for user win
+function checkDiagonal(){
+	if [[ ${board[0]} == ${board[4]} &&  ${board[4]} == ${board[8]} ]]
+	then
+			exit
+	elif [[ ${board[2]} == ${board[4]} &&  ${board[4]} == ${board[6]} ]]
+	then
+			exit
+	else
+			playerTurn $sign
 	fi
 }
 
 #Start Game
+viewBoard
 checkWhoPlayFirst
-checkValidCell
+playerTurn $sign
