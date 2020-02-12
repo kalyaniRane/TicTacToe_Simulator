a3e945129e1d90ccd9826916e0184b12ced5a0b9
kalyaniRane <kalyanirane19@gmail.com>
2020-02-12 11:47:24 +0530

    ADDED : Function To Check The Opponent Block

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -41,6 +41,7 @@ function checkValidCell(){
 
 #Function to play a user
 function userTurn(){
+	echo "User Turn"
 	if [[ $counter -lt 9 ]]
 	then
 		read -p "Enter a position to mark a sign: " position
@@ -49,8 +50,8 @@ function userTurn(){
 				board[position-1]=$1
 				((counter++))
 				viewBoard
-				checkWin $1 "User"	
-				echo "Computer Turn"
+				checkWin $player
+			echo "Computer Turn"
 				computerTurn $2 $1
 		else
 				echo "Choose Another Position"
@@ -65,118 +66,124 @@ function userTurn(){
 
 #Function to play a computer
 function computerTurn(){
+	flag=0
 	if [[ $counter -lt 9 ]]
 	then
-		checkRowWinning $1 $2
+		checkRowWinning $1 $2 "win"
+		checkRowWinning $2 $1 "block"
+	if [[ $flag -eq 0 ]]
+	then
 		position=$((RANDOM % 9 + 1))
 		if [[ $(checkValidCell) -eq 1 ]]
 		then
 				board[((position-1))]=$1
 				((counter++))
 				viewBoard
-				checkWin $1 "Computer"
+				checkWin $player
 				userTurn $2 $1
 		else
 				computerTurn $1 $2
 		fi
 	else
+			userTurn $2 $1
+	fi
+	else
 			echo "Game Over"
 			exit
 	fi
-	viewBoard
+	ViewBoard
 }
 
+#Function to set a sign for block and win conditions
+function checkBlockWin(){
+if [[ $3 == "block" ]]
+		then
+				sign=$2
+				player="User"
+		else
+				sign=$1
+				player="Computer"
+		fi
+}
+
+#Function to store value after checking Win or block condition
+function storeValue(){
+	board[$1]=$sign
+	((counter++))
+	viewBoard
+	flag=1
+}
 #Function to check Rows for Winning
 function checkRowWinning(){
+		checkBlockWin $1 $2 $3
 		for((i=0;i<9;i=i+3))
 		do
 			if [[ ${board[((i))]} == ${board[((i+1))]} && ${board[((i))]} == $1 && ${board[((i+2))]} == $((i+3)) ]]
 			then
-					board[((i+2))]=$1
-					((counter++))
-					viewBoard
+				storeValue $((i+2))				
 			elif [[ ${board[((i+1))]} == ${board[((i+2))]} &&  ${board[((i+1))]} == $1 && ${board[((i))]} == $((i+1)) ]]
 			then
-					board[((i))]=$1
-					((counter++))
-					viewBoard
+				storeValue $((i))
 			elif [[ ${board[((i))]} == ${board[((i+2))]} &&  ${board[((i))]} == $1 && ${board[((i+1))]} == $((i+2)) ]]
 			then
-					board[((i+1))]=$1
-					((counter++))
-					viewBoard
+				storeValue $((i+1))
 			fi
 		done
-		checkWin $1 "Computer"
-		checkColumnWin $1 $2
+		checkWin $player
+		checkColumnWin $1 $2 $3
 }
 
 #Function to check Columns for Winning
 function checkColumnWin(){
-
+	checkBlockWin $1 $2 $3
 	for((j=0;j<9;j++))
 	do
 			if [[ ${board[((j))]} == ${board[((j+3))]} &&  ${board[((j))]} == $1 && ${board[((j+6))]} == $((j+7)) ]]
 			then
-				board[((j+6))]=$1
-				((counter++))
-				viewBoard
+				storeValue $((j+6))
 			elif [[ ${board[((j+3))]} == ${board[((j+6))]} &&  ${board[((j+3))]} == $1 && ${board[((j))]} == $((j+1)) ]]
 			then
-				board[((j))]=$1
-				((counter++))
-				viewBoard
+				storeValue $((j))
 			elif [[ ${board[((j))]} == ${board[((j+6))]} &&  ${board[((j))]} == $1 && ${board[((j+3))]} == $((j+4)) ]]
 			then
-				board[((j+3))]=$1
-				((counter++))
-				viewBoard
+				storeValue $((j+3))
 		fi
 	done
-	checkWin $1 "Computer"
-	checkLeftDiagonal $1 $2
+	checkWin $player
+	checkLeftDiagonal $1 $2 $3
 }
 
 #Function to check left diagonal for Winning
 function checkLeftDiagonal(){
+	checkBlockWin $1 $2 $3
 	if [[ ${board[0]} == ${board[4]} && ${board[0]} == $1 && ${board[8]} == 9 ]]
 	then
-				board[8]=$1
-				((counter++))
-				viewBoard
+				storeValue 8
 	elif [[ ${board[0]} == ${board[8]} && ${board[0]} == $1 && ${board[4]} == 5 ]]
 	then
-				board[4]=$1
-				((counter++))
-				viewBoard
+				storeValue 4
 	elif [[ ${board[4]} == ${board[8]} && ${board[4]} == $1 && ${board[0]} == 1 ]]
 	then
-				board[0]=$1
-				((counter++))
-				viewBoard
+				storeValue 0
 	fi
-	checkRightDiagonal $1 $2
+	checkWin $player
+	checkRightDiagonal $1 $2 $3
 }
 
 #Function to check right diagonal fo Winning
 function checkRightDiagonal(){
+	checkBlockWin $1 $2 $3
 	if [[ ${board[2]} == ${board[4]} && ${board[2]} == $1 && ${board[6]} == 7 ]]
 	then
-				board[6]=$1
-				((counter++))
-				viewBoard
+				storeValue 6
 	elif [[ ${board[2]} == ${board[6]} && ${board[2]} == $1 && ${board[4]} == 5 ]]
 	then
-				board[4]=$1
-				((counter++))
-				viewBoard
+				storeValue 4
 	elif [[ ${board[4]} == ${board[6]} && ${board[4]} == $1 && ${board[2]} == 3 ]]
 	then
-				board[2]=$1
-				((counter++))
-				viewBoard
+				storeValue 2
 	fi
-	checkWin $1 "Computer"
+	checkWin $player
 }
 
 #Function to check rows and columns of player/computer is Win or not
@@ -187,38 +194,37 @@ function checkWin(){
 	do
 		if [[ ${board[((i))]} == ${board[((i+1))]} && ${board[((i+1))]} == ${board[((i+2))]} ]]
 		then
-				echo "$2 win"
+				echo "$1 win"
 				exit
 		elif [[ ${board[((j))]} == ${board[((j+3))]} && ${board[((j+3))]} == ${board[((j+6))]} ]]
 		then
-				echo "$2 win"
+				echo "$1 win"
 				exit
 		fi
 		((j++))
 	done
-	checkDiagonal $2 $1
+	checkDiagonal $1
 }
 
 #Function to check diagonal of player/computer is Win or not
 function checkDiagonal(){
 	if [[ ${board[0]} == ${board[4]} &&  ${board[4]} == ${board[8]} ]]
 	then
-			echo "$2 win"
+			echo "$1 win"
 			exit
 	elif [[ ${board[2]} == ${board[4]} &&  ${board[4]} == ${board[6]} ]]
 	then
-			echo "$2 win"
+			echo "$1 win"
 			exit
 	fi
 }
 
-#Start Game
-viewBoard
+#StartGame
 checkWhoPlayFirst
+viewBoard
 
 if [[ $user == 'X' ]]
 then
-	echo "User Turn"
 	userTurn $user $computer
 else
 	echo "Computer Turn"
